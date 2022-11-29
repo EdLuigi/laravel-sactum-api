8ac9da45a4967393a09d689f6f7f137158417d3c
Luigi <luigi.ciuffi@hotmail.com>
2022-11-29 20:14:53 -0300

    add rotas e controllers de update e destroy

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+// Route::get('/products', [ProductController::class, 'index']);
 
-Route::post('/products', [ProductController::class, 'store']);
+// Route::post('/products', [ProductController::class, 'store']);
+
+// Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::resource('products', ProductController::class);
